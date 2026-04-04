9bc0a47617ff49423166d9ac6fbf82ac40301488
Pimer0 <zefun74@gmail.com>
2026-04-04 18:40:19 +0200

    fix coment service

--- backoffice/src/Services/commentService.ts
+++ backoffice/src/Services/commentService.ts
@@ -1,28 +1,32 @@
 import { api } from "../lib/axios";
-import type { Comment, CreateCommentDto, UpdateCommentDto } from "../Types/CommentTypes";
+import type {
+	Comment,
+	CreateCommentDto,
+	UpdateCommentDto,
+} from "../Types/CommentTypes";
 
 export const commentService = {
-  getAll: async (): Promise<Comment[]> => {
-    const response = await api.get("/api/Comment");
-    return response.data;
-  },
+	getAll: async (): Promise<Comment[]> => {
+		const response = await api.get("/api/comments");
+		return response.data;
+	},
 
-  getById: async (id: string): Promise<Comment> => {
-    const response = await api.get(`/api/Comment/${id}`);
-    return response.data;
-  },
+	getById: async (id: string): Promise<Comment> => {
+		const response = await api.get(`/api/comments/${id}`);
+		return response.data;
+	},
 
-  create: async (params: CreateCommentDto): Promise<Comment> => {
-    const response = await api.post("/api/Comment", params);
-    return response.data;
-  },
+	create: async (params: CreateCommentDto): Promise<Comment> => {
+		const response = await api.post("/api/comments", params);
+		return response.data;
+	},
 
-  update: async (id: string, params: UpdateCommentDto): Promise<Comment> => {
-    const response = await api.put(`/api/Comment/${id}`, params);
-    return response.data;
-  },
+	update: async (id: string, params: UpdateCommentDto): Promise<Comment> => {
+		const response = await api.put(`/api/comments/${id}`, params);
+		return response.data;
+	},
 
-  delete: async (id: string): Promise<void> => {
-    await api.delete(`/api/Comment/${id}`);
-  },
+	delete: async (id: string): Promise<void> => {
+		await api.delete(`/api/comments/${id}`);
+	},
 };
